4de9081a1c5e53b945b52a84a062cd7dfe7af213
Roman Weintraub <roman.weintraub@googlemail.com>
2022-09-19 01:24:22 +0200

    create layout and dummy pages

--- inventory-frontend/pages/index.tsx
+++ inventory-frontend/pages/index.tsx
@@ -2,13 +2,20 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 const Home: NextPage = () => {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+  return (
+    <div className="hero">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Welcome to the Store Dashboard</h1>
+          <p className="py-6">You can look at the tomorrows deliveries, Delivered goods and all deliveries if needed</p>
+          <Link href="/deliveries/tomorrow"><button className="btn btn-neutral">Get Started</button></Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
