bf3539a1e991a1d115c5d111f4a6b9af6f4367ca
Roman Weintraub <roman.weintraub@googlemail.com>
2022-11-06 02:49:17 +0000

    add instructions to install cli tools

--- dev-env-warmup.sh
+++ dev-env-warmup.sh
@@ -3,3 +3,14 @@
 delivery-service/gradlw assemble
 store-service/gradlw assemble
 npm --prefix ./inventory-frontend install ./inventory-frontend
+
+# execute install script for terminal environment
+cd
+git clone https://github.com/Roni1993/.dotfiles
+cd .dotfiles
+./install
+
+# open zsh to install all zsh plugins
+chsh -s /usr/bin/zsh
+zsh
+zi update
