b4b9d64aa3181ad16388484acda3ee3d1a227697
Roman Weintraub <roman.weintraub@googlemail.com>
2022-11-06 15:22:03 +0000

    split warm up into two jobs

--- .space.kts
+++ .space.kts
@@ -1,11 +1,21 @@
-job("Warmup data for IDEA") {
+job("Warmup data for Fleet") {
     warmup(ide = Ide.Fleet) {
         // path to the warm-up script
         scriptLocation = "./dev-env-warmup.sh"
         // use image specified in the devfile
         // devfile = ".space/devfile.yaml"
     }
-    
+
+    // optional
+    git {
+        // fetch the entire commit history
+        depth = UNLIMITED_DEPTH
+        // fetch all branches
+        refSpec = "refs/*:refs/*"
+    }
+}
+
+job("Warmup data for IDEA") {
     warmup(ide = Ide.Idea) {
         // path to the warm-up script
         scriptLocation = "./dev-env-warmup.sh"
